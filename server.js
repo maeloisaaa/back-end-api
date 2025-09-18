eeb1f4213c4f8c05a3ec1fa6790074a22dc6c749
maeloisaaa <mecs3@aluno.ifnmg.edu.br>
2025-09-18 17:19:30 +0000

    ...

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
+import pkg from "pg";
+import dotenv from "dotenv";
+dotenv.config();         // Carrega e processa o arquivo .env
+const { Pool } = pkg;    // Utiliza a Classe Pool do Postgres
 import express from "express";
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
+
+app.get("/", async (req, res) => {
   console.log("Rota GET / solicitada");
+  const db = new Pool({
+    connectionString: process.env.URL_BD,
+  });
+  let dbStatus = "ok";
+  try {
+    await db.query("SELECT 1");
+  } catch (e) {
+    dbStatus = e.message;
+  }
   res.json({
     message: "API para controle de gastos pessoais",
     author: "Maria Eloísa Costa Silva",
+    statusBD: dbStatus
   });
 });
 
