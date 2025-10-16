ff063b0035f0e36a18fb4f9362efdf1adb690384
maeloisaaa <mecs3@aluno.ifnmg.edu.br>
2025-10-16 16:46:49 +0000

    ...

--- server.js
+++ server.js
@@ -18,7 +18,21 @@ function conectarBD() {
   }
   return pool;
 }
+app.get("/questoes", async (req, res) => {
 
+  const db = conectarBD();
+
+  try {
+    const resultado = await db.query("SELECT * FROM questoes"); // Executa uma consulta SQL para selecionar todas as questões
+    const dados = resultado.rows; // Obtém as linhas retornadas pela consulta
+    res.json(dados); // Retorna o resultado da consulta como JSON
+  } catch (e) {
+    console.error("Erro ao buscar questões:", e); // Log do erro no servidor
+    res.status(500).json({
+      erro: "Erro interno do servidor"
+    });
+  }
+});
 
 app.get("/", async (req, res) => {
   console.log("Rota GET / solicitada");
@@ -34,8 +48,8 @@ app.get("/", async (req, res) => {
   }
 
   res.json({
-    message: "API para questões",
-    author: "Maria Eloísa Costa Silva",
+    mensagem: "API para Questões de Prova", // Substitua pelo conteúdo da sua API
+    autor: "Maria Eloísa Costa Silva", // Substitua pelo seu nome
     dbStatus: dbStatus,
   });
 });
@@ -57,7 +71,32 @@ app.get("/questoes", async (req, res) => {
     });
   }
 });
+  // Rota para retornar uma questão pelo id
+  app.get("/questoes/:id", async (req, res) => {
+    console.log("Rota GET /questoes/:id solicitada"); // Log no terminal para indicar que a rota foi acessada
 
-app.listen(port, () => {
+    try {
+      const id = req.params.id; // Obtém o ID da questão a partir dos parâmetros da URL
+      const db = conectarBD(); // Conecta ao banco de dados
+      const consulta = "SELECT * FROM questoes WHERE id = $1"; // Consulta SQL para selecionar a questão pelo ID
+      const resultado = await db.query(consulta, [id]); // Executa a consulta SQL com o ID fornecido
+      const dados = resultado.rows; // Obtém as linhas retornadas pela consulta
+
+      // Verifica se a questão foi encontrada
+      if (dados.length === 0) {
+        return res.status(404).json({ mensagem: "Questão não encontrada" }); // Retorna erro 404 se a questão não for encontrada
+
+      }
+
+      res.json(dados); // Retorna o resultado da consulta como JSON
+    } catch (e) {
+      console.error("Erro ao buscar questão:", e); // Log do erro no servidor
+      res.status(500).json({
+        erro: "Erro interno do servidor"
+      });
+    }
+  });
+
+app.listen(port, '0.0.0.0', () => {
   console.log(`Serviço rodando na porta:  ${port}`);
 });
